8e7d529c09251bd14a8bac6208a916dc40b563e6
Darragh Murray <darragh.murray@gmail.com>
2023-05-19 08:28:39 +1000

    Update Week 1 Solution.sql

--- Week #1 - Danny's Diner/Week 1 Solution.sql
+++ Week #1 - Danny's Diner/Week 1 Solution.sql
@@ -1,6 +1,7 @@
----- WEEK 1: DANNY'S DINER--
+---- WEEK 1: DANNY'S DINER ----
 
 CREATE SCHEMA dannys_diner;
+SET search_path = dannys_diner;
 
 CREATE TABLE sales (
   "customer_id" VARCHAR(1),
@@ -53,11 +54,42 @@ VALUES
   ('A', '2021-01-07'),
   ('B', '2021-01-09');
 
-SELECT *
-FROM dbo.members;
+---- SOLUTIONS ----
 
-SELECT *
-FROM dbo.menu;
+---- Q1: What is the total amount each customer spent at the restaurant?
 
-SELECT *
-FROM dbo.sales;
+SELECT
+    sales.customer_id,
+    Sum(sales.product_id * menu.price) AS TotalSpent -- Multiply product number by its price and then sum all the values.
+FROM dannys_diner.menu
+INNER JOIN dannys_diner.sales
+ON dannys_diner.sales.product_id = dannys_diner.menu.product_id
+GROUP BY sales.customer_id
+ORDER BY  sales.customer_id, TotalSpent DESC;
+
+---- Q2: How many days has each customer visited the restaurant?
+
+SELECT
+    sales.customer_id,
+    COUNT(DISTINCT sales.order_date)
+FROM dannys_diner.sales
+GROUP BY sales.customer_id
+
+---- Q3. What was the first item from the menu purchased by each customer?
+
+WITH first_orders_cte AS
+(
+   SELECT customer_id, order_date, product_name,
+      DENSE_RANK() OVER(PARTITION BY a.customer_id
+      ORDER BY a.order_date) AS ranked_products
+   FROM dannys_diner.sales AS a
+   JOIN dannys_diner.menu AS b
+      ON a.product_id = b.product_id
+)
+
+SELECT customer_id, product_name
+FROM first_orders_cte
+WHERE ranked_products = 1
+GROUP BY customer_id, product_name;
+
+
